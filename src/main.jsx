c48aff4803abd965a7d0b7c5919318aa4275cbdf
leekli <li.kirkham@live.com>
2023-06-04 21:00:18 +0100

    swaps out fluentui framework for antd

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
-import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <FluentProvider theme={webLightTheme}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </FluentProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 )
