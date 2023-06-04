64ce524c0e7f30e3e835a793d5280c8c80795a20
leekli <li.kirkham@live.com>
2023-06-04 20:09:17 +0100

    first main commit

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,9 @@
+function Homepage() {
+    return (
+      <>
+        <h2>Homepage Header</h2>
+      </>
+    )
+}
+  
+export default Homepage
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx'
+import { FluentProvider, webLightTheme } from '@fluentui/react-components';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FluentProvider theme={webLightTheme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </FluentProvider>
   </React.StrictMode>,
 )
