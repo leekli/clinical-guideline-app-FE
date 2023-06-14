2787f57a254c611a1666dedcff02e3f2fdd027ca
leekli <li.kirkham@live.com>
2023-06-14 19:22:23 +0100

    removes link import from login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Alert, Button, Input, Tooltip } from "antd";
 import { InfoCircleOutlined, UserOutlined } from "@ant-design/icons";
 
