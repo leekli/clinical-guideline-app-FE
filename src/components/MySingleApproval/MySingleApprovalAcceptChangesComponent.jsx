f878c9847b50a344ebf6f2813dded7d53678e079
leekli <li.kirkham@live.com>
2023-07-08 19:41:29 +0100

    adds a final confirmation box for when an approval is about to be accepted and merged

--- src/components/MySingleApproval/MySingleApprovalAcceptChangesComponent.jsx
+++ src/components/MySingleApproval/MySingleApprovalAcceptChangesComponent.jsx
@@ -1,11 +1,12 @@
-import { Button } from "antd";
-import { CheckOutlined } from "@ant-design/icons";
+import { Button, Modal } from "antd";
+import { CheckOutlined, ExclamationCircleFilled } from "@ant-design/icons";
 import {
   deleteApprovalByName,
   deleteBranchByBranchName,
   patchGuidelineById,
 } from "../../utils/api-calls";
 import { useNavigate } from "react-router-dom";
+const { confirm } = Modal;
 
 export const MySingleApprovalAcceptChangesComponent = ({
   guideline,
@@ -21,32 +22,43 @@ export const MySingleApprovalAcceptChangesComponent = ({
     navigate(path);
   };
 
-  const handleSubmission = () => {
-    // 1. Setup PATCH request info to send and execute patch
-    const patchedGuideline = structuredClone(guideline);
+  const showConfirm = () => {
+    confirm({
+      title: "Are you sure that you want to approve this request?",
+      icon: <ExclamationCircleFilled />,
+      content:
+        "By approving this request, the changes will be merged into the main Guideline available for all users to read. It will also delete the current guideline workspace along with this approval request, once the Guideline merge has completed.",
+      onOk() {
+        // 1. Setup PATCH request info to send and execute patch
+        const patchedGuideline = structuredClone(guideline);
 
-    const dateNow = Date.now();
+        const dateNow = Date.now();
 
-    const submissionInfo = {
-      ChangeNumber: 0,
-      ChangeDescription: approvalPurpose,
-      ChangeOwner: branchOwner,
-      ChangeDatePublished: dateNow,
-    };
+        const submissionInfo = {
+          ChangeNumber: 0,
+          ChangeDescription: approvalPurpose,
+          ChangeOwner: branchOwner,
+          ChangeDatePublished: dateNow,
+        };
 
-    return patchGuidelineById(guidelineId, patchedGuideline, submissionInfo)
-      .then(() => {
-        // 2. delete relevant branch
-        return deleteBranchByBranchName(branchName);
-      })
-      .then(() => {
-        // 3. delete relevant approval
-        return deleteApprovalByName(approvalName);
-      })
-      .then(() => {
-        // 4. navigate back to approvals homepage
-        routeChange(`/myapprovals`);
-      });
+        return patchGuidelineById(guidelineId, patchedGuideline, submissionInfo)
+          .then(() => {
+            // 2. delete relevant branch
+            return deleteBranchByBranchName(branchName);
+          })
+          .then(() => {
+            // 3. delete relevant approval
+            return deleteApprovalByName(approvalName);
+          })
+          .then(() => {
+            // 4. navigate back to approvals homepage
+            routeChange(`/myapprovals`);
+          });
+      },
+      onCancel() {
+        console.log("Cancel");
+      },
+    });
   };
 
   return (
@@ -59,7 +71,7 @@ export const MySingleApprovalAcceptChangesComponent = ({
           background: "seagreen",
           borderColor: "black",
         }}
-        onClick={handleSubmission}
+        onClick={showConfirm}
       >
         Accept Approval Request and Merge Changes
       </Button>
